d2b7f9ba4753243f2651fc26c524f8feb5982f70
gooberbobber <dabamcdoodle@gmail.com>
2018-12-05 20:45:53 -0300

    Create main thread, Touch class and the GameView custom view

---
+++ app/src/main/java/com/raptor/raptor/gofar/GameView.java
@@ -0,0 +1,27 @@
+package com.raptor.raptor.gofar;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context) {
+        super(context);
+    }
+    public GameView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public GameView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+    void draw() {
+        invalidate();
+        requestLayout();
+    }
+}
--- app/src/main/java/com/raptor/raptor/gofar/MainActivity.java
+++ app/src/main/java/com/raptor/raptor/gofar/MainActivity.java
@@ -4,10 +4,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    GameView gameView;
+    Thread mainThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gameView = findViewById(R.id.gameView);
+        createAndStartMainThread();
+    }
+    private void createAndStartMainThread() {
+        mainThread = new Thread() {
+            public void run() {
+                while (mainThread.isAlive()) {
+                    long prevMillis = System.currentTimeMillis();
+                    try {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                gameView.draw();
+                            }
+                        });
+                        long waitTime = prevMillis - System.currentTimeMillis() + 30;
+                        if(waitTime > 0) {
+                            Thread.sleep(waitTime);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
+        mainThread.start();
     }
 }
